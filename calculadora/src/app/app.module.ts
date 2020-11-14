fe062ae90328ae5c8d2e870c911fe8c474d6fa3a
mathews19 <48871936+mathews19@users.noreply.github.com>
2020-11-13 21:04:57 -0300

    criando os services

--- calculadora/src/app/app.module.ts
+++ calculadora/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CalculadoraModule
+    CalculadoraModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- calculadora/src/app/calculadora/calculadora.module.ts
+++ calculadora/src/app/calculadora/calculadora.module.ts
@@ -1,12 +1,19 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CalculadoraComponent } from './components';
+import { CalculadoraService} from './services';
 
 @NgModule({
-  declarations: [],
+  declarations: [CalculadoraComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CalculadoraComponent
+  ],
+  providers: [
+    CalculadoraService
   ]
 })
 export class CalculadoraModule { }
